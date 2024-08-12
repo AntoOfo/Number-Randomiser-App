0698cf7f18c74ef71fcaa8c9f23c0c0090984651
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-08-12 21:54:38 +0100

    Functionality Working

--- app/src/main/java/com/example/numberrandomiser/MainActivity.kt
+++ app/src/main/java/com/example/numberrandomiser/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.numberrandomiser
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val rollButton = findViewById<Button>(R.id.rollBtn)
+        val resultText = findViewById<TextView>(R.id.resultsTextView)
+        val seekbar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = Random.nextInt(seekbar.progress)
+            resultText.text = rand.toString()       // turns result text to the random number and sets random numb to string
         }
+        }
+
     }
